30d0dde88568b8b4e2eb56b0239063f33ed2d2f0
ParkJunYong <pjy1418@gmail.com>
2019-03-22 23:56:05 +0900

    add luxurycar

--- AbstractFactory/Go/vehicle_factory.go
+++ AbstractFactory/Go/vehicle_factory.go
@@ -7,11 +7,20 @@ type VehicleFactory interface {
 
 const (
 	LuxuryCarType = 1
-	FamilyCarType =2
+	FamilyCarType = 2
 )
 
 
 type CarFactory struct{}
 func (c *CarFactory) NewVehicle(v int) (Vehicle, error){
-	
+	switch v {
+	case LuxuryCarType:
+		return new(LuxuryCar), nil
+	case FamilyCarType:
+		return new(FamilyCar), nil
+	default:
+		return nil,
+		errors.New(fmt.Sprintf("Vehicle of type %d not recognized\n", v))
+		
+	}
 }
