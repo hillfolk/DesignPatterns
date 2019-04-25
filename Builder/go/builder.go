61d35b869d384d39ac8c2f51c860803737e15020
ParkJunYong <pjy1418@gmail.com>
2019-04-26 08:23:49 +0900

    fix typo

--- Builder/go/builder.go
+++ Builder/go/builder.go
@@ -2,23 +2,23 @@ package builder
 
 // Builder 인터페이스 정의
 type Builder interface {
-	SetWheels(wheel int) Builder
-	SetSeats(seat int) Builder
-	SetStructure(structure string)Builder
+	SetWheels() Builder
+	SetSeats() Builder
+	SetStructure()Builder
 	GetVehicle() VehicleProduct
 }
 
 
-type ManufactureingDirector struct {
+type ManufacturingDirector struct {
 	builder Builder
 }
 
 
-func (f *ManufactureingDirector) Construct(s string) {
-	f.builder.SetSeats(3).SetStructure(s).SetWheels(4)
+func (f *ManufacturingDirector) Construct() {
+	f.builder.SetSeats().SetStructure().SetWheels()
 }
 
-func (f *ManufactureingDirector) SetBuilder(b Builder){
+func (f *ManufacturingDirector) SetBuilder(b Builder){
 	f.builder = b
 }
 
@@ -33,12 +33,69 @@ type CarBuilder struct {
 	v VehicleProduct
 }
 
-func (c *CarBuilder) SetWheels(wheel int) Builder {
-	c.v.Wheels = wheel
+func (c *CarBuilder) SetWheels() Builder {
+	c.v.Wheels = 4
+	return c;
 }
 
-func (c *CarBuilder) SetSeats(seat int) Builder {
-	c.v.Seats = seat
+func (c *CarBuilder) SetSeats() Builder {
+	c.v.Seats = 5
+	return c;
 }
 
+func (c *CarBuilder) SetStructure()Builder {
+	c.v.Structure = "Car"
+	return c;
+}
 
+func (c *CarBuilder) GetVehicle() VehicleProduct {
+	return c.v;
+}
+
+//A Builder of type motorbike
+type BikeBuilder struct {
+	v VehicleProduct
+}
+
+func (b *BikeBuilder) SetWheels() Builder {
+	b.v.Wheels = 2
+	return b
+}
+
+func (b *BikeBuilder) SetSeats() Builder {
+	b.v.Seats = 2
+	return b
+}
+
+func (b *BikeBuilder) SetStructure() Builder {
+	b.v.Structure = "Motorbike"
+	return b
+}
+
+func (b *BikeBuilder) GetVehicle() VehicleProduct {
+	return b.v
+}
+
+//A Builder of type motorbike
+type BusBuilder struct {
+	v VehicleProduct
+}
+
+func (b *BusBuilder) SetWheels() Builder {
+	b.v.Wheels = 8
+	return b
+}
+
+func (b *BusBuilder) SetSeats() Builder{
+	b.v.Seats = 30
+	return b
+}
+
+func (b *BusBuilder) SetStructure() Builder {
+	b.v.Structure = "Bus"
+	return b
+}
+
+func (b *BusBuilder) GetVehicle() VehicleProduct {
+	return b.v
+}
